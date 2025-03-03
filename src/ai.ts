d8d51815627d0b8612d7cb8e574e8b812c3714fe
bogdanbalan <bogdanbalan@microsoft.com>
2025-03-03 19:07:18 +0200

    Changes to replace OpenAI library with AzureOpenAI

--- src/ai.ts
+++ src/ai.ts
@@ -1,3 +1,12 @@
-import OpenAI from 'openai'
+import { AzureOpenAI } from 'openai'
 
-export const openai = new OpenAI()
+const endpoint = process.env.OPENAI_ENDPOINT || ''
+const azureApiKey = process.env.OPENAI_API_KEY || ''
+const deployment = process.env.AZURE_OPENAI_DEPLOYMENT_NAME || 'gpt-4o'
+
+export const openai = new AzureOpenAI({
+  endpoint: endpoint,
+  apiKey: azureApiKey,
+  apiVersion: '2025-01-01-preview',
+  deployment: deployment,
+})
