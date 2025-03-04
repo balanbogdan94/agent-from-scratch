058959d154a9528228ba53bde6fdb0a8e8084470
bogdanbalan <bogdanbalan@microsoft.com>
2025-03-04 12:06:52 +0200

    Added memory layer

---
+++ src/memory.ts
@@ -0,0 +1,43 @@
+import { v4 as getRandomUuid } from 'uuid'
+import type { AIMessage } from '../types'
+import { JSONFilePreset } from 'lowdb/node'
+
+export type MessageWithMetadata = AIMessage & {
+  id: string
+  createdAt: string
+}
+
+export const addMetadata = (message: AIMessage): MessageWithMetadata => ({
+  ...message,
+  id: getRandomUuid(),
+  createdAt: new Date().toISOString(),
+})
+
+export const removeMetadata = (message: MessageWithMetadata): AIMessage => {
+  const { id, createdAt, ...rest } = message
+  return rest
+}
+
+type Data = {
+  messages: MessageWithMetadata[]
+}
+
+const initialData: Data = {
+  messages: [],
+}
+
+const getDB = async () => {
+  const db = await JSONFilePreset<Data>('db.json', initialData)
+  return db
+}
+
+export const getMessages = async () => {
+  const db = await getDB()
+  return db.data.messages.map(removeMetadata)
+}
+
+export const addMessage = async (message: AIMessage[]) => {
+  const db = await getDB()
+  db.data.messages.push(...message.map(addMetadata))
+  await db.write()
+}
